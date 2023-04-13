71278747c610333a29dde6e8722c613a772694ac
Alfi Nashri <alfi.nashri1@gmail.com>
2023-04-13 15:35:26 +0700

    Add files via upload

---
+++ database/migrations/2023_04_08_104242_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_kelas')->unique();
+            $table->string('nama_kelas');
+            $table->string('wali_kelas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
